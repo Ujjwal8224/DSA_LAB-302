89597b0c727a3532e4cf1601b032a1eaf103fcc1
Ujjwal8224 <ujjwal.kumar.cse.2025@miet.ac.in>
2026-07-28 11:58:51 +0530

    Add files via upload
    FIRST

---
+++ first.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(){
+    int n, i, arr[100];
+    printf("Enter no. of elements:\n");
+    scanf("%d", &n);
+    printf("Enter elements: \n");
+    for(i=0;i<n;i++){
+      scanf("%d", &arr[i]);
+    }
+    printf("Print ele: \n");
+    for( i= 0;i<n;i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
